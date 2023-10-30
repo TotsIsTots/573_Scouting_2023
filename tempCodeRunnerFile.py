43f171d0d71533a4825876957a4d6db7bef310c5
ThomasPinto2134 <145403603+ThomasPinto2134@users.noreply.github.com>
2023-10-30 19:48:52 -0400

    best commit

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,1 @@
-defense = UI_Elements.Dropdown(20, 450, 380, 30, [
-                                   "Didn't Play", "Played Poorly", "Played Okay", "Played Good", "Played Great", "Unknown"], 'Defense', title_size=32)
+ * .05, reset_rect.y - ((reset_render.get_height() - action_buttons_size) / 2)))
